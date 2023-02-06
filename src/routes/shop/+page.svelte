9dd718a85457e60aa637499f448dc9712868d42f
Christophe Harrer <c.harrer@prolificlab.com>
2023-02-06 11:10:52 +0100

    Update +page.svelte

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-3xl">This is going to be my shop one day. i willcall it Shop</h1>
+<h1 class="text-3xl">This is going to be my shop one day. I will call it Shop.</h1>
 <style>
     h1{
         color:#258974;
